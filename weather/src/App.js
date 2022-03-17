f716eec28e04ca9b07c775f641f21fc9cd94d23c
Perla-Al-Haddad <perlaalhaddad@gmail.com>
2022-03-17 12:20:54 +0200

    added section scrolling

--- weather/src/App.js
+++ weather/src/App.js
@@ -15,6 +15,7 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { faBarChart, faAreaChart, faPieChart, faProjectDiagram } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -24,10 +25,11 @@ import ForceChart from './Components/ChartComponents/ForceChart/ForceChart';
 
 import Footer from './Components/Footer/Footer';
 import ChartBox from './Components/ChartBox/ChartBox';
-
 import StatisticsContainer from './Components/StatisticContainer/StatisticContainer'
+import Section from './Components/Section/Section';
+import ScrollToButton from './Components/ScrollToButton/ScrollToButton';
 
-library.add(faBarChart, faAreaChart, faPieChart, faProjectDiagram);
+library.add(faBarChart, faAreaChart, faPieChart, faProjectDiagram, faCircle);
 
 const drawerWidth = 240;
 
@@ -144,13 +146,24 @@ function DashboardContent() {
                   </Typography>
                 </MenuItem>
                 <MenuItem >
-                  <Typography className='text-white' textAlign="center" variant={'h6'}>Navigation Item 1 <FontAwesomeIcon icon={["fas", "fa-chart-pie"]} /></Typography>
+                  <Typography className='text-white' textAlign="center" variant={'h6'}>
+                    <ScrollToButton toId="CoreStatisticsDIV">Core Statistics <FontAwesomeIcon icon={["far", "fa-circle"]} /></ScrollToButton>
+                  </Typography>
                 </MenuItem>
                 <MenuItem >
-                  <Typography className='text-white' textAlign="center" variant={'h6'}>Navigation Item 2 <FontAwesomeIcon icon={['fas', 'fa-chart-bar']} /></Typography>
+                  <Typography className='text-white' textAlign="center" variant={'h6'}>
+                    <ScrollToButton toId="Chart1DIV">Chart Container 1 <FontAwesomeIcon icon={["fas", "fa-chart-pie"]} /></ScrollToButton>
+                  </Typography>
                 </MenuItem>
                 <MenuItem >
-                  <Typography className='text-white' textAlign="center" variant={'h6'}>Navigation Item 3 <FontAwesomeIcon icon={['fas', 'fa-project-diagram']} /></Typography>
+                  <Typography className='text-white' textAlign="center" variant={'h6'}>
+                    <ScrollToButton toId="Chart2DIV">Chart Container 2 <FontAwesomeIcon icon={["fas", "fa-chart-bar"]} /></ScrollToButton>
+                  </Typography>
+                </MenuItem>
+                <MenuItem >
+                  <Typography className='text-white' textAlign="center" variant={'h6'}>
+                    <ScrollToButton toId="Chart3DIV">Chart Container 3 <FontAwesomeIcon icon={["fas", "fa-project-diagram"]} /></ScrollToButton>
+                  </Typography>
                 </MenuItem>
               </Grid>
             </Container>
@@ -173,29 +186,37 @@ function DashboardContent() {
                 </ChartBox>
               </Grid>
               <Grid item xs={12} md={12} lg={12} >
-                <ChartBox headerName="Core Statistics">
-                  <Grid container sx={{p:3, ml:0}} spacing={5}>
-                    <StatisticsContainer title="Stat Title" value="541,687$" year="2020"/>
-                    <StatisticsContainer title="Stat Title" value="18,127$" year="2020"/>
-                    <StatisticsContainer title="Stat Title" value="21" year="2020"/>
-                    <StatisticsContainer title="Stat Title" value="NAN" year="2020"/>
-                  </Grid>
-                </ChartBox>
+                <Section id={"CoreStatisticsDIV"}>
+                  <ChartBox headerName="Core Statistics">
+                    <Grid container sx={{ p: 3, ml: 0 }} spacing={5}>
+                      <StatisticsContainer title="Stat Title" value="541,687$" year="2020" />
+                      <StatisticsContainer title="Stat Title" value="18,127$" year="2020" />
+                      <StatisticsContainer title="Stat Title" value="21" year="2020" />
+                      <StatisticsContainer title="Stat Title" value="NAN" year="2020" />
+                    </Grid>
+                  </ChartBox>
+                </Section>
               </Grid>
               <Grid item xs={12} md={8} lg={7} >
-                <ChartBox headerName="Chart Title 1">
-                  <PieChart height={680} />
-                </ChartBox>
+                <Section id={'Chart1DIV'}>
+                  <ChartBox headerName="Chart Title 1">
+                    <PieChart height={680} />
+                  </ChartBox>
+                </Section>
               </Grid>
               <Grid item xs={12} md={4} lg={5} >
-                <ChartBox headerName="Chart Title 2">
-                  <BarChart height={680} />
-                </ChartBox>
+                <Section id={'Chart2DIV'}>
+                  <ChartBox headerName="Chart Title 2">
+                    <BarChart height={680} />
+                  </ChartBox>
+                </Section>
               </Grid>
               <Grid item xs={12}>
-                <ChartBox headerName="Chart Title 3">
-                  <ForceChart height={680} />
-                </ChartBox>
+                <Section id={'Chart3DIV'}>
+                  <ChartBox headerName="Chart Title 3">
+                    <ForceChart height={680} />
+                  </ChartBox>
+                </Section>
               </Grid>
             </Grid>
           </Container>
---
+++ weather/src/Components/Section/Section.jsx
@@ -0,0 +1,11 @@
+// Section.jsx
+
+import React from "react";
+
+const ScrollToButton = React.forwardRef(({ id, children }, ref) => (
+  <section ref={ref} id={id}>
+    {children}
+  </section>
+));
+
+export default ScrollToButton;
